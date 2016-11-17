1e898e89e9ba13f8ab04af8846143a405179fc3c
poornimasewak <poornima.mathur.20@gmail.com>
2016-11-16 18:00:34 -0600

    bootstrap

--- Knight/js/app.js
+++ Knight/js/app.js
@@ -1,6 +1,6 @@
  var a = 'chicago';
  var queryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAZ-hvbSlP3mny8PzPV_FHMlvZX6OIEDwM&callback=initMap";
-  
+  // var queryURL = "https://developers.zomato.com/api/v2.1/cities=chicago&user-key=2fb021c464c36ea4538d72f0ab37aee4&limit=10";
             //ajax call with done function
 	        $.ajax({
 	            url: queryURL,
