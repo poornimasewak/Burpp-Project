51f88d129733793bb012c375d4eaae6833888976
poornimasewak <poornima.mathur.20@gmail.com>
2016-11-17 15:42:46 -0600

    working api data

--- Knight/js/app.js
+++ Knight/js/app.js
@@ -1,13 +1,50 @@
- var a = 'chicago';
- var queryURL = "https://crossorigin.me/https://api.edamam.com/search?q=chicken&app_id=e5ee4c7d&app_key=c8fc66f63a363261369faadc4fdd29ae&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free";
- 			//make a variable and get rid of search 'chicken'
-            //ajax call with done function
-	        $.ajax({
-	            url: queryURL,
-	            method: 'GET'
-	        })
-	        .done(function(response) {
+$(document).ready(function() {
 
-	            console.log(response);
-	            var results = response.data;
-	        });
+    // Capture Button Click
+    $("#btn-food").on("click", function() {
+        // Grabbed value from text boxes
+        fname = $("#food-search").val().trim();
+        console.log("food: " + fname);
+
+        // The following 4 variables are in the order they need to be 
+        // concatenated in var queryURL
+        var crossoriginURL = "https://crossorigin.me/";
+        var edamamURL = "https://api.edamam.com/search?";
+        var search = "q=" + fname;
+        var keys = "&app_id=e5ee4c7d&app_key=c8fc66f63a363261369faadc4fdd29ae";
+
+        var queryURL = crossoriginURL + edamamURL + search + keys;
+
+        var config = { url: queryURL, method: 'GET' };
+        console.log("url before query: " + queryURL);
+
+        $.ajax({ url: queryURL, method: 'GET' })
+            .done(function(response) {
+                console.log(".done: " + response);
+
+                $('.div-recipe-area').empty();
+
+                for (var i = 0; i < response.hits.length; i++) {
+                    console.log(response.hits[i].recipe.label);
+                    console.log(response.hits[i].recipe.image);
+
+                    var recipeDiv = $('<div class="recipe">');
+
+                    var p = $('<h5>').text("Name: " + response.hits[i].recipe.label);
+
+                    var recipeImage = $('<img>');
+                    recipeImage.attr('src', response.hits[i].recipe.image);
+                    recipeImage.attr('float', 'left');
+
+                    recipeDiv.append(p);
+                    recipeDiv.append(recipeImage);
+
+                    $('.div-recipe-area').append(recipeDiv);
+                }
+            });
+
+        // Don't refresh the page!
+        return false;
+    });
+
+});
