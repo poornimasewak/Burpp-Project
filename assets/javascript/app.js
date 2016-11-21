edc17f50cde0a98618eabaae0149f7745311e645
mmeder2016 <mmeder2016@gmail.com>
2016-11-20 23:18:44 -0600

    Test area for yelp query development. Added 2 new files: assets/lib/oauth.js and sha.js. Added 3 new script lines to head in index.html. Work in progress. Still has small problems which need to be solved.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,167 @@
+$(document).ready(function() {
 
+    var fname = "";
+
+    // Capture Button Click
+    $("#btn-food").on("click", function() {
+
+        // Grabbed values from text boxes
+        var fname = $("#food-search").val().trim();
+        var loc = $("#location").val().trim();
+        console.log("food: " + fname);
+
+        var response = yelpQuery(fname, loc, "10");
+        return false;
+    });
+
+    function yelpQuery(food, loc, num) {
+
+        function cb(data) {
+            makeYelpDiv(data);
+        }
+        // the callback function cb needs to be global, the following line does this
+        window.cb = cb;
+
+        var auth = {
+            consumerKey: "3EYbFFuD4X44tV01t3Nbiw",
+            consumerSecret: "a71F0PmRXr0O1vpRHiIyxaxepYI",
+            accessToken: "z-WvclgEHnhaYrCTFVaTPUi5hbw36zxS",
+            accessTokenSecret: "8uwuZ5AE3aQpwU3K_PYLXHEOeq4",
+            serviceProvider: {
+                signatureMethod: "HMAC-SHA1"
+            }
+        };
+
+        var terms = food;
+        var near = loc;
+        var accessor = {
+            consumerSecret: auth.consumerSecret,
+            tokenSecret: auth.accessTokenSecret
+        };
+
+        var parameters = [];
+        parameters.push(['term', terms]);
+        parameters.push(['location', near]);
+        parameters.push(['limit', num]);
+        parameters.push(['callback', 'cb']);
+        parameters.push(['oauth_consumer_key', auth.consumerKey]);
+        parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+        parameters.push(['oauth_token', auth.accessToken]);
+        parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+        var message = {
+            'action': 'http://api.yelp.com/v2/search',
+            'method': 'GET',
+            'parameters': parameters,
+            'dataType': 'jsonp'
+        };
+
+        OAuth.setTimestampAndNonce(message);
+        OAuth.SignatureMethod.sign(message, accessor);
+
+        var parameterMap = OAuth.getParameterMap(message.parameters);
+
+        $.ajax({
+                'url': message.action,
+                'data': parameterMap,
+                'dataType': 'jsonp',
+                'jsonpCallback': 'cb',
+                'cache': true
+            })
+            .done(function(data, textStatus, jqXHR) {
+                console.log('success[' + data + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+            });
+    }
+
+    // Create div representing all of the businesses from the Yelp response
+    // We take the javascript JSON object and 'i', the index number of the business in 
+    // the (javascript JSON object).businesses[i]. See the html below this function 
+    // to see the structure of the div that will be created
+    function makeYelpDiv(jsJsonObj) {
+        // For each business returned from the Yelp query
+        for (var i = 0; i < jsJsonObj.businesses.length; i++) {
+            // Get the individual business data
+            var business = jsJsonObj.businesses[i];
+
+            // The primary div with a heading and collapsible panel
+            var divYelpBusiness = $('<div class="panel panel-default div-yelp-business">');
+
+            // 1.) create div-yelp-heading
+            var divYelpHeading = $('<div class="panel-heading div-yelp-heading">');
+            var h4 = $('<h4 class="panel-title">');
+            var a = $('<a data-toggle="collapse" href="#collapse1">');
+            a.text(business.name);
+            var rating = $('<img >');
+            rating.attr("src", business.rating_img_url);
+
+            h4.append(a);
+            divYelpHeading.append(h4);
+            divYelpHeading.append(rating);
+            divYelpBusiness.append(divYelpHeading);
+            // 1.) div-yelp-heading complete and appended to div-yelp-business
+
+            // 2.) create div collapse
+            var divCollapse = $('<div id="collapse1" class="panel-collapse collapse">');
+            var divYelpBody = $('<div class="panel-body div-yelp-body">');
+            // BEGIN DISPLAYED RESTAURANT DATA
+            //divYelpBody.append(business.name);
+            var phone = formatPhone(business.phone);
+
+            divYelpBody.append(phone).append('<br>');
+            if(business.neighborhoods !== undefined) {
+                for (var j = 0; j < business.location.neighborhoods.length; j++) {
+                    divYelpBody.append(business.location.neighborhoods[j]).append('<br>');
+                }
+            }
+            for (var k = 0; k < business.location.display_address.length; k++) {
+                divYelpBody.append(business.location.display_address[k]).append('<br>');
+            }
+            var strURL=$('<a>').attr("href", business.url).append("Yelp Link");
+            divYelpBody.append(strURL);
+            // END DISPLAYED RESTAURANT DATA
+
+            divCollapse.append(divYelpBody);
+            divYelpBusiness.append(divCollapse);
+            // 2.) div collapse complete and appended to div-yelp-business
+
+            $(".div-yelp").append(divYelpBusiness);
+        }
+    }
+    /*    <div class="panel-body div-yelp">
+            <div class="panel panel-default div-yelp-business">
+                <div class="panel-heading div-yelp-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" href="#collapse1" class="collapsed">
+                            Sapori Napoletani
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse1" class="panel-collapse collapse">
+                    <div class="panel-body div-yelp-body">
+                        Sapori Napoletani
+                    </div>
+                </div>
+            </div>
+        </div>
+    */
+
+    /**
+     *  Format phone numbers
+     */
+    function formatPhone(phonenum) {
+        var regexObj = /^(?:\+?1[-. ]?)?(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/;
+        if (regexObj.test(phonenum)) {
+            var parts = phonenum.match(regexObj);
+            var phone = "";
+            if (parts[1]) { phone += "+1 (" + parts[1] + ") "; }
+            phone += parts[2] + "-" + parts[3];
+            return phone;
+        } else {
+            //invalid phone number
+            return phonenum;
+        }
+    }
+});
