8c2ae8ca1f04c4ac19b2a9d31351d618daf6a837
mmeder2016 <mmeder2016@gmail.com>
2016-11-23 21:35:16 -0600

    Attempting to integrate yelp and edamam into the project on a separate branch

--- Knight/js/app.js
+++ Knight/js/app.js
@@ -36,12 +36,19 @@ $(document).ready(function() {
         return false;
     });
 
+
     // Capture Button Click
     $("#btn-food").on("click", function() {
-        // Grabbed value from text boxes
-        fname = $("#food-search").val().trim();
-        console.log("food: " + fname);
+        // Grabbed values from text boxes
+        var fname = $("#food-search").val().trim();
+        var loc = $("#location").val().trim();
 
+        yelpQuery(fname, loc, "10");
+        edamamQuery(fname);
+        return false;
+    });
+
+    function edamamQuery(fname) {
         // The following 4 variables are in the order they need to be 
         // concatenated in var queryURL
         var crossoriginURL = "https://crossorigin.me/";
@@ -51,34 +58,28 @@ $(document).ready(function() {
 
         var queryURL = crossoriginURL + edamamURL + search + keys;
 
-        var config = {
-            url: queryURL,
-            method: 'GET'
-        };
+        var config = { url: queryURL, method: 'GET' };
         console.log("url before query: " + queryURL);
 
-        $.ajax({
-                url: queryURL,
-                method: 'GET'
-            })
+        $.ajax({ url: queryURL, method: 'GET' })
             .done(function(response) {
                 console.log(".done: " + response);
 
                 $('.div-recipe-area').empty();
-
+                makeRecipeDivHeading();
                 for (var i = 0; i < response.hits.length; i++) {
                     makeRecipeDiv(response, i);
                 }
             });
+    }
 
-        // Don't refresh the page!
-        return false;
-    });
+    function makeRecipeDivHeading() {
+        var divRecipeHeading = $('<div class="panel-heading">');
+        divRecipeHeading.html('<h3 class="panel-title">Edamam Recipes</h3>');
+        $(".div-recipe-area").append(divRecipeHeading);
+    }
 
     function makeRecipeDiv(response, i) {
-
-        var p = $('<h5>').text(response.hits[i].recipe.label);
-
 
         var divIndividualRecipe = $('<div class="panel panel-default div-recipe">');
         // divIndividualRecipe is a panel that contains a panel-heading and a panel-body
@@ -87,9 +88,6 @@ $(document).ready(function() {
         // divRecipeBody contains an aside left and an aside right
         var asideLeft = $('<aside class="aside-left-image">');
         var asideRight = $('<aside class="aside-right-recipe-content">');
-
-        // var p = $('<h5>').text(response.hits[i].recipe.label);
-
 
         var recipe = response.hits[i].recipe;
 
@@ -107,7 +105,6 @@ $(document).ready(function() {
 
         var ingredientList = $('<ol>');
 
-
         for (var i = 0; i < recipe.ingredientLines.length; i++) {
             var li = $('<li>').text(recipe.ingredientLines[i]);
             ingredientList.append(li);
@@ -124,9 +121,157 @@ $(document).ready(function() {
         divIndividualRecipe.append(divHeading).append(divRecipeBody);
 
         $(".div-recipe-area").append(divIndividualRecipe);
-        //creating button to save recipes
-        // var saveRecipeButton = $('<button>').text("Save Recipe");
-        // asideRight.append(saveRecipeButton);
-
     }
+
+    function yelpQuery(food, loc, num) {
+
+        function cb(data) {
+            makeYelpDiv(data);
+        }
+        // the callback function cb needs to be global, the following line does this
+        window.cb = cb;
+
+        var auth = {
+            consumerKey: "3EYbFFuD4X44tV01t3Nbiw",
+            consumerSecret: "a71F0PmRXr0O1vpRHiIyxaxepYI",
+            accessToken: "z-WvclgEHnhaYrCTFVaTPUi5hbw36zxS",
+            accessTokenSecret: "8uwuZ5AE3aQpwU3K_PYLXHEOeq4",
+            serviceProvider: {
+                signatureMethod: "HMAC-SHA1"
+            }
+        };
+
+        var terms = food;
+        var near = loc;
+        var accessor = {
+            consumerSecret: auth.consumerSecret,
+            tokenSecret: auth.accessTokenSecret
+        };
+
+        var parameters = [];
+        parameters.push(['term', terms]);
+        parameters.push(['location', near]);
+        parameters.push(['limit', num]);
+        parameters.push(['callback', 'cb']);
+        parameters.push(['oauth_consumer_key', auth.consumerKey]);
+        parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+        parameters.push(['oauth_token', auth.accessToken]);
+        parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+        var message = {
+            'action': 'http://api.yelp.com/v2/search',
+            'method': 'GET',
+            'parameters': parameters,
+            'dataType': 'jsonp'
+        };
+
+        OAuth.setTimestampAndNonce(message);
+        OAuth.SignatureMethod.sign(message, accessor);
+
+        var parameterMap = OAuth.getParameterMap(message.parameters);
+
+        $.ajax({
+                'url': message.action,
+                'data': parameterMap,
+                'dataType': 'jsonp',
+                'jsonpCallback': 'cb',
+                'cache': true
+            })
+            .done(function(data, textStatus, jqXHR) {
+                console.log('success[' + data + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+            });
+    }
+
+    function makeYelpDivHeading() {
+        var divYelpHeading = $('<div class="panel-heading">');
+        divYelpHeading.html('<h3 class="panel-title">Yelp Restaurants</h3>');
+        $(".div-yelp").append(divYelpHeading);
+    }
+
+    // Create div representing all of the businesses from the Yelp response
+    // We take the javascript JSON object and 'i', the index number of the business in 
+    // the (javascript JSON object).businesses[i]. See the html below this function 
+    // to see the structure of the div that will be created
+    function makeYelpDiv(jsJsonObj) {
+        $('.div-yelp').empty();
+        makeYelpDivHeading();
+        // For each business returned from the Yelp query
+        for (var i = 0; i < jsJsonObj.businesses.length; i++) {
+            // Get the individual business data
+            var business = jsJsonObj.businesses[i];
+            // Create a panel id for each restaurant panel
+            var panelId = "collapse" + i.toString();
+
+            // The primary div with a heading and collapsible panel
+            var divYelpBusiness = $('<div class="panel panel-default div-yelp-business">');
+
+            // 1.) create div-yelp-heading
+            var divYelpHeading = $('<div class="panel-heading div-yelp-heading">');
+            var h4 = $('<h4 class="panel-title">');
+            var aStr = '<a data-toggle="collapse" href="#' + panelId + '">';
+            var a = $(aStr);
+            a.text(business.name);
+            var rating = $('<img >');
+            rating.attr("src", business.rating_img_url);
+            var img_restaurant = $('<img >');
+            img_restaurant.attr("src", business.image_url);
+
+            h4.append(a);
+            divYelpHeading.append(h4);
+            divYelpHeading.append(rating);
+            divYelpHeading.append('<br>');
+
+            divYelpHeading.append(img_restaurant);
+            divYelpBusiness.append(divYelpHeading);
+            // 1.) div-yelp-heading complete and appended to div-yelp-business
+
+            // 2.) create div collapse
+            var divStr = '<div id="' + panelId + '" class="panel-collapse collapse">';
+            var divCollapse = $(divStr);
+            var divYelpBody = $('<div class="panel-body div-yelp-body">');
+            // BEGIN DISPLAYED RESTAURANT DATA
+            //divYelpBody.append(business.name);
+            var phone = formatPhone(business.phone);
+
+            divYelpBody.append(phone).append('<br>');
+            if (business.neighborhoods !== undefined) {
+                for (var j = 0; j < business.location.neighborhoods.length; j++) {
+                    divYelpBody.append(business.location.neighborhoods[j]).append('<br>');
+                }
+            }
+            for (var k = 0; k < business.location.display_address.length; k++) {
+                divYelpBody.append(business.location.display_address[k]).append('<br>');
+            }
+            var strURL = $('<a>').attr("href", business.url).append("Yelp Link");
+            divYelpBody.append(strURL);
+            // END DISPLAYED RESTAURANT DATA
+
+            divCollapse.append(divYelpBody);
+            divYelpBusiness.append(divCollapse);
+            // 2.) div collapse complete and appended to div-yelp-business
+
+            $(".div-yelp").append(divYelpBusiness);
+        }
+    }
+
+    /**
+     *  Format phone numbers
+     */
+    function formatPhone(phonenum) {
+        var regexObj = /^(?:\+?1[-. ]?)?(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/;
+        if (regexObj.test(phonenum)) {
+            var parts = phonenum.match(regexObj);
+            var phone = "";
+            if (parts[1]) { phone += "+1 (" + parts[1] + ") "; }
+            phone += parts[2] + "-" + parts[3];
+            return phone;
+        } else {
+            //invalid phone number
+            return phonenum;
+        }
+    }
+
 });
