7f042a41549f10c07f4dd79f6afc55fedf208c50
Emily Turner-Sherwood <emilyturnerr@gmail.com>
2016-11-29 21:56:19 -0600

    heroku

--- Knight/js/app.js
+++ Knight/js/app.js
@@ -356,7 +356,7 @@ $(document).ready(function() {
         parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
 
         var message = {
-            'action': 'http://api.yelp.com/v2/search',
+            'action': 'https://api.yelp.com/v2/search',
             'method': 'GET',
             'parameters': parameters,
             'dataType': 'jsonp'
